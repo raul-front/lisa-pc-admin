ea6574331e541b30eb722b1c1f2f7a2d8f5048ac
raul <18919015125@163.com>
2021-06-23 17:38:02 +0800

    add init file

---
+++ filter/const.js
@@ -0,0 +1,15 @@
+/*
+ * @Descripttion: 过滤器数据
+ * @Author: pujianguo
+ * @Date: 2021-04-02 14:46:34
+ *
+ * 通过filter()方法转换
+ * eg: filter(0, 'cardType'), 结果为身份证
+ */
+
+// 性别
+export const sexData = {
+  0: '女',
+  1: '男',
+  default: '保密',
+}
